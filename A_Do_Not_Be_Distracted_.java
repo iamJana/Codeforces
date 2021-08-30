4b8388bd3895fe7d37f621fd88fb59c3888ebc54
jana <janamurugesan01@gmail.com>
2021-08-30 12:32:04 +0530

    Updated

---
+++ A_Do_Not_Be_Distracted_.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/**
+ * A_Do_Not_Be_Distracted_
+ */
+public class A_Do_Not_Be_Distracted_ {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = in.nextInt();
+            String s = in.next();
+            char[] arr = s.toCharArray();
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[j] + " ");
+            }
+            for(int k=0;k<arr.length;k++)
+            for (int j = 0; j < arr.length; j++) {
+                
+            }
+
+            in.close();
+        }
+    }
+}
